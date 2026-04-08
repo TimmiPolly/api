090162c263ce53ef4418ae9a358e7c182a1f6ac2
Timofei Blohin <121554655+TimmiPolly@users.noreply.github.com>
2026-04-08 13:59:59 +0300

    Update level2.worker.js

--- level2.worker.js
+++ level2.worker.js
@@ -29,7 +29,7 @@ export default {
   },
 };
 
-// ====================== ПОЛУЧЕНИЕ ДАННЫХ ======================3
+// ====================== ПОЛУЧЕНИЕ ДАННЫХ ======================4
 async function level2() {
   try {
     const response = await fetch('https://cryptottlivewebapi.free2ex.net:8443/api/v2/public/level2/BTCUSDT?depth=1', {
